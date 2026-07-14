8c873ed5c884f09feda53e3c9353db1bf9d1f18c
shashi26g <shashi2668g@gmail.com>
2026-07-14 20:27:09 +0000

    fix: incorporate maven-jar-plugin manifest parameters and keep-alive loop

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,12 @@
 package com.example;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Java GitOps App running successfully inside Amazon EKS!");
+        
+        while (true) {
+            System.out.println("Application heartbeat check: OK");
+            Thread.sleep(60000); 
+        }
     }
 }
